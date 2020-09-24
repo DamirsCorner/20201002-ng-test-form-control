867c679d37aa689571569361f5881c97e23f3805
Damir Arh <damir.arh@gmail.com>
2020-09-24 12:29:31 +0200

    No value accessor error in test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule, NgSelectModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
